14508b6cb1ac1ed9caf7fe7ff47686d2364f7d26
davean <davean@xkcd.com>
2014-05-18 17:41:21 -0400

    New secondary index for doc.

--- src/CodeSearch/DocumentQuery.hs
+++ src/CodeSearch/DocumentQuery.hs
@@ -2,13 +2,17 @@ module CodeSearch.DocumentQuery
   ( query
   ) where
 
-import           CodeSearch.Index    (DocIndex, queryIndex)
+import           CodeSearch.Index    (DocIndex, queryIndex, getA)
 import           CodeSearch.Regexp   (parseRegexp)
 import           CodeSearch.Trigrams (build)
 import           CodeSearch.Types    (Document, Query (..), RegExpr)
 
+import           Data.Maybe
 import           Control.Applicative
-import           Data.Set            (Set, intersection, union)
+import           Data.Set            (Set)
+import qualified Data.Set as Set
+import           Data.IntSet (IntSet)
+import qualified Data.IntSet as ISet
 import           Data.Text           (Text, pack)
 
 eitherToMaybe :: Either a b -> Maybe b
@@ -16,25 +20,26 @@ eitherToMaybe (Left _) = Nothing
 eitherToMaybe (Right x) = Just x
 
 query :: DocIndex -> Text -> Maybe (Set Document)
-query idx txt = eitherToMaybe (parseRegexp txt) >>= compile
+query idx txt =
+    fmap (ISet.foldl' (\a -> Set.union a . maybe Set.empty Set.singleton . getA idx) Set.empty) ((eitherToMaybe (parseRegexp txt)) >>= compile)
   where
-    compile :: RegExpr -> Maybe (Set Document)
+    compile :: RegExpr -> Maybe IntSet
     compile = reduce . fmap (queryIndex idx . pack) . build
 
 -- Reduce a Query over sets of documents to either a single set or Nothing,
 -- which represents our inability to reduce the search space.
-reduce :: Query (Set Document) -> Maybe (Set Document)
+reduce :: Query IntSet -> Maybe IntSet
 reduce q = case reduce' q of
              Any -> Nothing
              Val a -> Just a
              q' -> error ("Could not fully reduce " ++ show q')
 
-reduce' :: Query (Set Document) -> Query (Set Document)
+reduce' :: Query IntSet -> Query IntSet
 reduce' Any           = Any
 reduce' (Val a)       = Val a
 reduce' (Any `Or` _)  = Any
 reduce' (_ `Or` Any)  = Any
-reduce' (s `Or` s')   = union <$> reduce' s  <*> reduce' s'
+reduce' (s `Or` s')   = ISet.union <$> reduce' s  <*> reduce' s'
 reduce' (Any `And` s) = reduce' s
 reduce' (s `And` Any) = reduce' s
-reduce' (s `And` s')  = intersection <$> reduce' s <*> reduce' s'
+reduce' (s `And` s')  = ISet.intersection <$> reduce' s <*> reduce' s'
--- src/CodeSearch/Index.hs
+++ src/CodeSearch/Index.hs
@@ -1,7 +1,8 @@
 module CodeSearch.Index
   ( Index(..)
   , DocIndex
-  , mapIndex
+  , getA
+--  , mapIndex
   , queryIndex
   , singleton
   ) where
@@ -11,10 +12,12 @@ import           CodeSearch.Types
 import           Control.Applicative
 import           Data.Map.Strict  (Map, union)
 import qualified Data.Map.Strict  as Map
-import           Data.Maybe       (fromMaybe)
+import           Data.Bimap (Bimap)
+import qualified Data.Bimap as Bimap
+import           Data.Maybe       (fromMaybe, fromJust)
 import           Data.Monoid
-import           Data.Set         (Set)
-import qualified Data.Set         as Set
+import           Data.IntSet         (IntSet)
+import qualified Data.IntSet         as ISet
 import           Data.Text        (Text, drop, length, take)
 import           Prelude          hiding (drop, length, take)
 import           Data.Bytes.Serial
@@ -23,25 +26,40 @@ type DocIndex = Index Document
 
 type Trigram = Text
 
-newtype Index a = Index (Map Trigram (Set a))
-  deriving (Eq, Show, Read)
+data Index a =
+    Index (Bimap a Int) (Map Trigram IntSet)
+  deriving (Eq, Show)
 
 instance (Ord a, Serial a) => Serial (Index a) where
-  serialize (Index i) = serialize i
-  deserialize = Index <$> deserialize
+  serialize (Index s i) = serialize (Bimap.toList s) >> serialize i
+  deserialize = Index <$> (Bimap.fromList <$> deserialize) <*> deserialize
 
-mapIndex :: (Ord a, Ord b) => (a -> b) -> Index a -> Index b
-mapIndex f (Index idx) = Index $ (Map.map . Set.map) f idx
+getA :: Ord a => Index a -> Int -> Maybe a
+getA (Index s _) i = Bimap.lookupR i s
 
+repInt :: Map Int Int -> Map Trigram IntSet -> Map Trigram IntSet
+repInt l = Map.map (ISet.map (\v -> fromJust $ Map.lookup v l))
+
+{-mapIndex :: (Ord a, Ord b) => (a -> b) -> Index a -> Index b
+mapIndex f (Index s idx) =
+    let (t, ns) = Map.foldlWithKey (\(t, i) d o ->
+                                        let n = f d
+                                            ci = Map.insertWith 
+                                        in (Map.insert )) s
+    in Index ns (repInt t idx)
+-}
 instance Ord a => Monoid (Index a) where
-  mempty = Index mempty
-  mappend (Index idx) (Index idx') = Index (Map.unionWith Set.union idx idx')
+  mempty = Index Bimap.empty mempty
+  mappend (Index s idx) (Index s' idx') =
+    let newS = Bimap.fold (\a b c -> Bimap.tryInsert a (Bimap.size c) c) s s'
+        newIdx = (Map.unionWith ISet.union idx (Map.map (ISet.map (\i -> fromJust (Bimap.lookupR i s' >>= (flip Bimap.lookup $ newS)))) idx'))
+    in newS `seq` newIdx `seq` Index newS newIdx
 
 singleton :: Document -> Text -> DocIndex
 singleton = addToIndex mempty
 
-queryIndex :: DocIndex -> Trigram -> Set Document
-queryIndex (Index idx) tri = fromMaybe Set.empty (Map.lookup tri idx)
+queryIndex :: DocIndex -> Trigram -> IntSet
+queryIndex (Index _ idx) tri = fromMaybe ISet.empty (Map.lookup tri idx)
 
 addToIndex :: DocIndex -> Document -> Text -> DocIndex
 addToIndex idx doc txt | length trigram < 3 = idx
@@ -49,4 +67,4 @@ addToIndex idx doc txt | length trigram < 3 = idx
   where
     trigram = take 3 txt
     nexts   = drop 1 txt
-    single  = Index (Map.singleton trigram (Set.singleton doc))
+    single  = Index (Bimap.singleton doc 0) (Map.singleton trigram (ISet.singleton 0))
